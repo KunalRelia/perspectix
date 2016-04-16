a9f6ee8540f9461a97f49fba22149a88614ebaa6
Sanjitha <Sanjitha12345@gmail.com>
2016-04-16 19:26:37 -0400

    Usage of Geo Google Maps for fetching address for a given lat-lon.

---
+++ UI/js/getAddress.js
@@ -0,0 +1,16 @@
+
+function fetchAddress(latitude,longitude,marker) {
+	
+    var geocoder = new google.maps.Geocoder();  
+    
+    myLatLng = {lat: latitude, lng: longitude};
+    geocoder.geocode({'location': myLatLng}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) { 
+            marker.bindPopup("Address : "+results[0].formatted_address);
+        } else {
+            console.log('Geocode was not successful for the following reason: ' + status);
+            marker.bindPopup("Location Unavailable");
+        }
+    });  
+    
+}
