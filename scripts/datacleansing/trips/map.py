1f02ad2758d811d019f46a92da993cf587d9503b
Sanjitha <Sanjitha12345@gmail.com>
2016-05-05 02:46:02 -0400

    Cleaning script for Trips:Check for valid dates)

--- scripts/datacleansing/trips/map.py
+++ scripts/datacleansing/trips/map.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 
 import sys, os
+from datetime import datetime
+
+def validDate(date):
+	d = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+	now = datetime.now()
+
+	return now > d
+
+def compareDates(pickup,dropoff):
+	p = datetime.strptime(pickup, "%Y-%m-%d %H:%M:%S")
+	d = datetime.strptime(dropoff, "%Y-%m-%d %H:%M:%S")
+
+       	return p <= d
+
 
 for line in sys.stdin:
     if "trip" in line:
         continue
-    print line.strip()
+    values =line.split(',')
+    pickupDate = values[5]
+    dropoffDate = values[6]
+
+    if validDate(dropoffDate) and validDate(pickupDate) and compareDates(pickupDate,dropoffDate):
+    	print line.strip()
