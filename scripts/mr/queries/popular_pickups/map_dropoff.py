b2c560b63906a43dd116fd8e304cdfe7dbbbe7c5
Abhishek Pillai <abhipil@gmail.com>
2016-05-07 07:03:48 -0400

    MapRed queries: FEAT, FIX: trips per taxi per day, popular locations

---
+++ scripts/mr/queries/popular_pickups/map_dropoff.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import sys
+
+
+def convert_to_date(val):
+    return val.strip().split(' ')[0]
+
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    line = line.strip()
+
+    key, values = line.split('\t', 1)
+
+    keys = key.split(',')
+    pickup_date = convert_to_date(keys[3])
+
+    values = values.split(',')
+    try:
+        pickup = list(map(float, values[8:10]))
+    except ValueError:
+        continue
+
+    try:
+        passenger_count, trip_time, trip_dist = list(map(int, values[3:6]))
+        total_amt, total_toll, tip_amt = (int(values[-1]), int(values[-2]), int(values[-3]))
+    except ValueError:
+        continue
+    payment_type = values[-7].strip()
+    print '%s,%0.3f,%0.3f\t%d,%d,%d,%d,%d,%d,%s' % (pickup_date, pickup[0], pickup[1],
+                                                    passenger_count, trip_time, trip_dist, total_amt,
+                                                    total_toll, tip_amt, payment_type)
---
+++ scripts/mr/queries/trips_per_taxi_per_day/reduce.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import sys
+
+current_key = None
+trip_count = 0
+current_num_passengers = 0
+current_total_time = 0
+current_total_distance = 0
+current_total_amount = 0
+current_total_toll = 0
+current_total_tips = 0
+current_cash_payments = 0
+current_card_payments = 0
+
+
+def emit():
+    print "%s\t%d,%d,%d,%d,%d,%d,%d,%d,%d" % (current_key, trip_count, current_num_passengers, current_total_time,
+                                              current_total_distance, current_total_amount, current_total_toll,
+                                              current_total_tips, current_cash_payments, current_card_payments)
+
+
+print "pickup_date,medallion\ttotal_trips,total_passengers_driven,total_time_driven,total_distance,total_amount," \
+      "total_toll_paid,total_tips_collected,total_cash_payments_received,total_card_payments_received"
+
+# input comes from STDIN (stream data that goes to the program)
+for line in sys.stdin:
+
+    key, values = line.strip().split("\t", 1)
+    values = values.strip().split(',', 6)
+    payment = values[-1]
+    try:
+        values = list(map(int, values[:-1]))
+    except ValueError:
+        continue
+    if len(values) != 6:
+        continue
+    if key == current_key:
+        trip_count += 1
+        current_num_passengers += values[0]
+        current_total_time += values[1]
+        current_total_distance += values[2]
+        current_total_amount += values[3]
+        current_total_toll += values[4]
+        current_total_tips += values[5]
+        if "CSH" in payment:
+            current_cash_payments += 1
+        if "CRD" in payment:
+            current_card_payments += 1
+    else:
+        if current_key:
+            emit()
+        current_key = key
+        trip_count = 1
+        current_num_passengers = values[0]
+        current_total_time = values[1]
+        current_total_distance = values[2]
+        current_total_amount = values[3]
+        current_total_toll = values[4]
+        current_total_tips = values[5]
+        if "CSH" in payment:
+            current_cash_payments = 1
+        if "CRD" in payment:
+            current_card_payments = 1
+
+emit()
