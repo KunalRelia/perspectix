293b113b0051e415ded44bd65e36c8df8e5ee927
KunalRelia <kunal_relia91@hotmail.com>
2016-05-07 06:51:54 -0400

    corrected trips_per_day/map.py

--- scripts/mr/queries/trips_per_day/map.py
+++ scripts/mr/queries/trips_per_day/map.py
@@ -13,14 +13,29 @@ for line in sys.stdin:
 
     key, columns = line.split('\t', 1)
     keys = key.split(',')
-    values=columns.split(',')
+    values = columns.split(',')
 
     pick_up_date_time = keys[3]
     day = convert_to_date(pick_up_date_time)
 
-    total_distance = int(columns[5])
-    tips = int(columns[-3])
-    toll = int(columns[-2])
-    total_amount = int(columns[-1])
+    try:
+        total_distance = int(values[5])
+    except ValueError:
+        total_distance = 0
+
+    try:
+        tips = int(values[-3])
+    except ValueError:
+        tips = 0
+
+    try:
+        toll = int(values[-2])
+    except ValueError:
+        toll = 0
+
+    try:
+        total_amount = int(values[-1])
+    except ValueError:
+        total_amount = 0
 
     print '%s\t%d,%d,%d,%d' % (day, total_distance, tips, toll, total_amount)
